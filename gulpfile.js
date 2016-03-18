a7b7ced9de90a98ecbbc104dd276502073b6bfac
Maira Bello <maira.araujo@liferay.com>
2016-03-18 16:15:24 -0300

    Fixes a few problems when running the bundle in a closure

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp');
 var header = require('gulp-header');
 var runSequence = require('run-sequence');
 var minify = require('gulp-minify');
+var replace = require('gulp-replace');
 
 var src = [
   'node_modules/google-closure-library/closure/goog/base.js',
@@ -60,6 +61,7 @@ var src = [
 gulp.task('build', function() {
   return gulp.src(src)
     .pipe(concat('bundle.js'))
+    .pipe(replace('var goog = goog || {};', 'var goog = this.goog || {};'))
     .pipe(header('this.CLOSURE_NO_DEPS = true;\nthis.goog = this.goog || {};\n\n'))
     .pipe(footer('\n\ngoog.loadModule(function() {\n' +
       '  goog.module(\'incrementaldom\');\n' +
--- vendor/incremental-dom.js
+++ vendor/incremental-dom.js
@@ -19,9 +19,7 @@
  */
 
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.IncrementalDOM = global.IncrementalDOM || {})));
+  factory((global.IncrementalDOM = global.IncrementalDOM || {}));
 }(this, function (exports) { 'use strict';
 
   /**
