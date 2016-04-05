da5ea07f38ae01a3a74e68a84f67c768e73db304
Maira Bello <maira.araujo@liferay.com>
2016-04-05 14:06:14 -0300

    Imports incremental-dom from bundle

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,13 @@ var concat = require('gulp-concat');
 var footer = require('gulp-footer');
 var gulp = require('gulp');
 var header = require('gulp-header');
-var runSequence = require('run-sequence');
-var minify = require('gulp-minify');
 var replace = require('gulp-replace');
 
 gulp.task('build', function() {
   return gulp.src('manuallyBuiltDeps.js')
     .pipe(concat('bundle.js'))
     .pipe(replace('var goog = goog || {};', 'var goog = this.goog || {};'))
-    .pipe(header('(function() {\nthis.CLOSURE_NO_DEPS = true;\nthis.goog = this.goog || {};\n\n'))
+    .pipe(header('import \'metal-incremental-dom\';\n\n(function() {\nthis.CLOSURE_NO_DEPS = true;\nthis.goog = this.goog || {};\n\n'))
     .pipe(footer('\n\ngoog.loadModule(function() {\n' +
       '  goog.module(\'incrementaldom\');\n' +
       '  return IncrementalDOM;\n' +
@@ -22,12 +20,4 @@ gulp.task('build', function() {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('minify', function() {
-  return gulp.src('build/bundle.js')
-    .pipe(minify())
-    .pipe(gulp.dest('build'));
-});
-
-gulp.task('default', function(done) {
-  runSequence('build', 'minify', done);
-});
+gulp.task('default', ['build']);
