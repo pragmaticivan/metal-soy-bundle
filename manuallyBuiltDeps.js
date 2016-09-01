3dd511de18af2a654c53570dac68f34bf2e4a296
Maira Bello <maira.araujo@liferay.com>
2016-09-01 10:55:22 -0300

    Adds new function from soyutils's new version

--- manuallyBuiltDeps.js
+++ manuallyBuiltDeps.js
@@ -4418,21 +4418,27 @@ goog.inherits(goog.soy.data.UnsanitizedText, goog.soy.data.SanitizedContent);
    *     additional mappings.
    */
   soy.$$augmentMap = function(baseMap, additionalMap) {
-
-    // Create a new map whose '__proto__' field is set to baseMap.
-    /** @constructor */
-    function TempCtor() {}
-    TempCtor.prototype = baseMap;
-    var augmentedMap = new TempCtor();
-
-    // Add the additional mappings to the new map.
-    for (var key in additionalMap) {
-      augmentedMap[key] = additionalMap[key];
-    }
-
-    return augmentedMap;
+    return soy.$$assignDefaults(soy.$$assignDefaults({}, additionalMap), baseMap);
   };
 
+
+  /**
+   * Copies extra properties into an object if they do not already exist. The
+   * destination object is mutated in the process.
+   *
+   * @param {!Object} obj The destination object to update.
+   * @param {!Object} defaults An object with default properties to apply.
+   * @return {!Object} The destination object for convenience.
+   */
+  soy.$$assignDefaults = function(obj, defaults) {
+    for (var key in defaults) {
+      if (!(key in obj)) {
+        obj[key] = defaults[key];
+      }
+    }
+
+    return obj;
+  };
 
   /**
    * Checks that the given map key is a string.
